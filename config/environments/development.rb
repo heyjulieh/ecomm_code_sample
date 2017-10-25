0bf7ee3c644f7f71afe36b20b88b78059e3de353
heyjulieh <heyjulieh@gmail.com>
2017-10-25 11:25:48 -0700

    config redis for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,13 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  if ENV['REDIS_URL']
+    config.action_controller.perform_caching = true
+    config.cache_store = :redis_store
+  else
+    config.action_controller.perform_caching = false
+  end
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
