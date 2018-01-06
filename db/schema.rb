1803b575656f1f6d5add363c307c3d4e2d431978
heyjulieh <heyjulieh@gmail.com>
2018-01-05 17:14:36 -0800

    reconfirm works local but not heroku.. need to figure out why

--- db/schema.rb
+++ db/schema.rb
@@ -78,10 +78,8 @@ ActiveRecord::Schema.define(version: 20171031220138) do
     t.string "zip_code"
     t.string "country"
     t.string "state"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
