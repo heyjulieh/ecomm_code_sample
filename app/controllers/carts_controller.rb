930114f09fc53036a2ef21feab1fb2aaaadc26de
heyjulieh <heyjulieh@gmail.com>
2018-01-25 14:08:37 -0800

    edit and delete functionality for items completed

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -16,7 +16,6 @@ class CartsController < ApplicationController
       item.update_columns(status: item.status = "Selling in Progress")
       item.update_columns(countdown: item.countdown = 600)
       current_user.countdown(600)
-    # elsif item.quantity == 0
     end
   end
 
