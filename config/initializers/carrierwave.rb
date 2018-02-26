3421fd2ecc34db011316914bc58c2b7a50aac587
heyjulieh <heyjulieh@gmail.com>
2018-02-25 20:25:41 -0800

    fixing carrier wave bug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,16 @@
 CarrierWave.configure do |config|
 
+  config.fog_credentials = {
+    :provider               => 'AWS',                             # required
+    :aws_access_key_id      => 'AKIAJHKGH545PHASA26Q',            # required
+    :aws_secret_access_key  => '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',     # required
+    :region                 => 'us-west-2'                        # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'oddofficialimages'               # required
+  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+  #config.fog_public     = false                                  # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
@@ -14,16 +25,7 @@ CarrierWave.configure do |config|
     end
   end
 
-  config.fog_credentials = {
-    :provider               => 'AWS',                             # required
-    :aws_access_key_id      => 'AKIAJHKGH545PHASA26Q',            # required
-    :aws_secret_access_key  => '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',     # required
-    :region                 => 'us-west-2'                        # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'oddofficialimages'               # required
-  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-  #config.fog_public     = false                                  # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+
 end
 
 # module CarrierWave
