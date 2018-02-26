693f1455eb18218f8c9807faf9064f1efb9535a2
heyjulieh <heyjulieh@gmail.com>
2018-02-25 19:22:32 -0800

    removed registration create code for now-- need to fix because it is causing a sign up bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,16 +4,16 @@ class RegistrationsController < Devise::RegistrationsController
     super
   end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      current_user.move_to(@user) if current_user && current_user.guest?
-      sign_up("user", @user)
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @user = User.new(params[:user])
+  #   if @user.save
+  #     current_user.move_to(@user) if current_user && current_user.guest?
+  #     sign_up("user", @user)
+  #     redirect_to root_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
   private
 
