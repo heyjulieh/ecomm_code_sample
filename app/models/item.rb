028900e9f38799f46196a991db609cd34b132876
heyjulieh <heyjulieh@gmail.com>
2018-03-16 11:19:29 -0700

    removed colorway validation for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   has_many :purchases
   has_many :buyers, through: :purchases
   validates :brand, presence: true
-  validates :colorway, presence: true
   validates :description, presence: true
   validates :product_name, presence: true
   validates :price, presence: true
